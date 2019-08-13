dd0bd7020abb1f8d2afb4aa9f0132e36e464d433
JonGomer <jongomer22@gmail.com>
2019-08-12 21:00:38 -0700

    api key setup and storage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,29 @@
     // The width and height of the captured photo. We will set the
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
-  
+
     var width = 320;    // We will scale the photo width to this
     var height = 0;     // This will be computed based on the input stream
-  
+
     // |streaming| indicates whether or not we're currently streaming
     // video from the camera. Obviously, we start at false.
-  
+
     var streaming = false;
-  
+
     // The various HTML elements we need to configure or control. These
     // will be set by the startup() function.
-  
+
     var video = null;
     var canvas = null;
     var photo = null;
     var startbutton = null;
-  
+
     function startup() {
       video = document.getElementById('video');
       canvas = document.getElementById('canvas');
       photo = document.getElementById('photo');
       startbutton = document.getElementById('startbutton');
-  
+
       navigator.mediaDevices.getUserMedia({video: true, audio: false})
       .then(function(stream) {
         video.srcObject = stream;
@@ -33,18 +33,18 @@
       .catch(function(err) {
         console.log("An error occurred: " + err);
       });
-  
+
       video.addEventListener('canplay', function(ev){
         if (!streaming) {
           height = video.videoHeight / (video.videoWidth/width);
-        
+
           // Firefox currently has a bug where the height can't be read from
           // the video, so we will make assumptions if this happens.
-        
+
           if (isNaN(height)) {
             height = width / (4/3);
           }
-        
+
           video.setAttribute('width', width);
           video.setAttribute('height', height);
           canvas.setAttribute('width', width);
@@ -52,40 +52,40 @@
           streaming = true;
         }
       }, false);
-  
+
       startbutton.addEventListener('click', function(ev){
         takepicture();
         ev.preventDefault();
       }, false);
-      
+
       clearphoto();
     }
-  
+
     // Fill the photo with an indication that none has been
     // captured.
-  
+
     function clearphoto() {
       var context = canvas.getContext('2d');
       context.fillStyle = "#AAA";
       context.fillRect(0, 0, canvas.width, canvas.height);
-  
+
       var data = canvas.toDataURL('image/png');
       photo.setAttribute('src', data);
     }
-    
+
     // Capture a photo by fetching the current contents of the video
     // and drawing it into a canvas, then converting that to a PNG
     // format data URL. By drawing it on an offscreen canvas and then
     // drawing that to the screen, we can change its size and/or apply
     // other changes before drawing it.
-  
+
     function takepicture() {
       var context = canvas.getContext('2d');
       if (width && height) {
         canvas.width = width;
         canvas.height = height;
         context.drawImage(video, 0, 0, width, height);
-      
+
         var data = canvas.toDataURL('image/png');
         photo.setAttribute('src', data);
         console.log(photo)
@@ -93,7 +93,7 @@
         clearphoto();
       }
     }
-  
+
     // Set up our event listener to run the startup process
     // once loading is complete.
     window.addEventListener('load', startup, false);
@@ -108,11 +108,31 @@
     }).then(function(response){
         console.log(JSON.stringify(response));
         let joke = response.joke;
-        let twoPart = 
+        let twoPart =
         `Setup: ${response.setup} <br>
         Delivery: ${response.delivery}`;
-    
+
         $("#joke").html(joke || twoPart);
     });
     console.log(jokeURL);
+
   };
+
+  // API that pulls music
+
+function musicURL(){
+  letmusicURL = "SpotifyPublicAPIdimasV1.p.rapidapi.com"
+
+  $.ajax({
+  url: musicURL,
+  method: "GET",
+  }).then(function(response){
+  console.log(JSON.stringify(response));
+  let music = response.music;
+
+  //We need to create a 'div' with an ID of 'music'
+
+  $("#music").html(music);
+  });
+  console.log(musicURL);
+  }
