c2f833b7f3a744e0eaec21a858edec59a024a55f
manzav95 <zavalamanuel95@gmail.com>
2019-08-14 21:14:20 -0700

    Setup FaceAPI to console.log emotion and linked to live stream picture taking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,138 +1,215 @@
 (function() {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
+  // The width and height of the captured photo. We will set the
+  // width to the value defined here, but the height will be
+  // calculated based on the aspect ratio of the input stream.
 
-    var width = 320;    // We will scale the photo width to this
-    var height = 0;     // This will be computed based on the input stream
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
 
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
 
-    var streaming = false;
+  var streaming = false;
 
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
 
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
 
-    function startup() {
-      video = document.getElementById('video');
-      canvas = document.getElementById('canvas');
-      photo = document.getElementById('photo');
-      startbutton = document.getElementById('startbutton');
+  function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('selfie');
+    startbutton = document.getElementById('startbutton');
 
-      navigator.mediaDevices.getUserMedia({video: true, audio: false})
-      .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function(err) {
-        console.log("An error occurred: " + err);
-      });
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function(err) {
+      console.log("An error occurred: " + err);
+    });
 
-      video.addEventListener('canplay', function(ev){
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth/width);
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
 
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
 
-          if (isNaN(height)) {
-            height = width / (4/3);
-          }
-
-          video.setAttribute('width', width);
-          video.setAttribute('height', height);
-          canvas.setAttribute('width', width);
-          canvas.setAttribute('height', height);
-          streaming = true;
+        if (isNaN(height)) {
+          height = width / (4/3);
         }
-      }, false);
 
-      startbutton.addEventListener('click', function(ev){
-        takepicture();
-        ev.preventDefault();
-      }, false);
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
 
-      clearphoto();
-    }
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
 
-    // Fill the photo with an indication that none has been
-    // captured.
+    clearphoto();
+  }
 
-    function clearphoto() {
-      var context = canvas.getContext('2d');
-      context.fillStyle = "#AAA";
-      context.fillRect(0, 0, canvas.width, canvas.height);
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
 
       var data = canvas.toDataURL('image/png');
       photo.setAttribute('src', data);
+      console.log(photo)
+    } else {
+      clearphoto();
     }
+  }
 
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener('load', startup, false);
+})();
 
-    function takepicture() {
-      var context = canvas.getContext('2d');
-      if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
-
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data);
-        console.log(photo)
-      } else {
-        clearphoto();
-      }
-    }
-
-    // Set up our event listener to run the startup process
-    // once loading is complete.
-    window.addEventListener('load', startup, false);
-  })();
-
-  function getJokes(){
-    let jokeURL = "https://sv443.net/jokeapi/category/dark"
-
-    $.ajax({
-        url: jokeURL,
-        method: "GET",
-    }).then(function(response){
-        console.log(JSON.stringify(response));
-        let joke = response.joke;
-        let twoPart =
-        `Setup: ${response.setup} <br>
-        Delivery: ${response.delivery}`;
-
-        $("#joke").html(joke || twoPart);
-    });
-    console.log(jokeURL);
-
-  };
-
-  // API that pulls music
-
-function musicURL(){
-  letmusicURL = "SpotifyPublicAPIdimasV1.p.rapidapi.com"
+//API to get a joke
+function getJokes(){
+  let jokeURL = "https://sv443.net/jokeapi/category/programming"
 
   $.ajax({
-  url: musicURL,
-  method: "GET",
+      url: jokeURL,
+      method: "GET",
   }).then(function(response){
-  console.log(JSON.stringify(response));
-  let music = response.music;
+      console.log(JSON.stringify(response));
+      let joke = response.joke;
+      let twoPart =
+      `Setup: ${response.setup} <br>
+      Delivery: ${response.delivery}`;
 
-  //We need to create a 'div' with an ID of 'music'
-
-  $("#music").html(music);
+      $("#joke").html(joke || twoPart);
   });
-  console.log(musicURL);
+  console.log(jokeURL);
+
+};
+
+// API that pulls music
+
+function musicURL(){
+letmusicURL = "SpotifyPublicAPIdimasV1.p.rapidapi.com"
+
+$.ajax({
+url: musicURL,
+method: "GET",
+}).then(function(response){
+console.log(JSON.stringify(response));
+let music = response.music;
+
+//We need to create a 'div' with an ID of 'music'
+
+$("#music").html(music);
+});
+console.log(musicURL);
+}
+
+function readImage() {
+
+  let filesSelected = document.getElementById("selfie").files;
+  if (filesSelected.length > 0) {
+    let fileToLoad = filesSelected[0];
+
+    let fileReader = new FileReader();
+
+    fileReader.onload = function(fileLoadedEvent) {
+      srcData = fileLoadedEvent.target.result; // <--- data: base64
+
+      let newImage = document.createElement('img');
+      newImage.src = srcData;
+
+      document.getElementById("imgDisplay").innerHTML = newImage.outerHTML;
+    }
+    fileReader.readAsDataURL(fileToLoad);
   }
+}
+function faceEmotion() {
+  let selfieImg = $("#selfie").attr("src");
+
+  console.log(selfieImg);
+
+  $.ajax({
+    url: "https://api-us.faceplusplus.com/facepp/v3/detect",
+    method: "POST",
+    data: {api_key: "pieTMPdJke7_Im-gUG0sIetqrMESk7Jl", api_secret: "52wvsEmp0s22NTWTP7hxgJbTIdwu7p1I", image_base64: selfieImg, return_attributes: "emotion,gender,age"},
+    dataType: "json",
+    error: faceEmotion
+  }).then(function(response) {
+
+    let json = JSON.stringify(response);
+    let resultObject = JSON.parse(json);
+    console.log(resultObject);
+
+    let sadness = resultObject.faces[0].attributes.emotion.sadness;
+    let neutral = resultObject.faces[0].attributes.emotion.neutral;
+    let disgust = resultObject.faces[0].attributes.emotion.disgust;
+    let anger = resultObject.faces[0].attributes.emotion.anger;
+    let surprise = resultObject.faces[0].attributes.emotion.surprise;
+    let fear = resultObject.faces[0].attributes.emotion.fear;
+    let happiness = resultObject.faces[0].attributes.emotion.happiness;
+
+  //   let gender = resultObject.faces[0].attributes.gender.value;
+  //   let age = resultObject.faces[0].attributes.age.value;
+
+    console.log("Sadness: " + sadness);
+    console.log("Neutral: " + neutral);
+    console.log("Disgust: " + disgust);
+    console.log("Anger: " + anger);
+    console.log("Surprise: " + surprise);
+    console.log("Fear: " + fear);
+    console.log("Happiness: " + happiness);
+
+
+    if (sadness > neutral && sadness > happiness) {
+     console.log("sad life, cheer up");
+     console.log("Sadness: " + sadness);
+
+    } else if (happiness > neutral && happiness > sadness) {
+      console.log("Real happy, calm down");
+      console.log("Happiness: " + happiness);
+
+    } else if (anger > happiness && anger > sadness) {
+        console.log("You MAD mad");
+        console.log("Anger: " + anger);
+
+    }
+  
+  });
+
+}
+
+$(document).on("click", "#startbutton", faceEmotion);
